6c41ac38738f7cbfcfc4d10620d6fb6a5f083f87
Natan Schons <natanschons@gmail.com>
2025-02-17 21:42:46 -0300

    novas predicoes com query

--- projeto_pontos/churn_model/predict.py
+++ projeto_pontos/churn_model/predict.py
@@ -1,13 +1,25 @@
 # %%
 
 import pandas as pd
+import os
+# %%
 
 model = pd.read_pickle("modelo_rf.pkl")
 model
 # %%
 
-df = pd.read_csv('../data/dados_pontos.csv',sep=';')
+df = pd.read_csv('../../data/dados_pontos.csv',sep=';')
 df.head()
+# %%
+
+dt = '2024-05-08'
+
+with open('etl_model.sql', 'r') as open_file:
+    query = open_file.read()
+
+query = query.format(date=dt)
+
+print(query)
 # %%
 
 X = df[model['features']]
@@ -18,4 +30,9 @@ df['prob_active'] = predict_proba
 df[['Name','prob_active']]
 # %%
 model
+# %%
+os.chdir('../projeto_pontos/churn_model')# %%
+
+# %%
+print(os.listdir())  # Lista os arquivos e pastas no diretório atual
 # %%
