cb27256f9934a9ca908b3f87e6741e5b9f59dd5b
Natan Schons <natanschons@gmail.com>
2025-02-17 21:11:09 -0300

    new predict

--- projeto_pontos/churn_model/predict.py
+++ projeto_pontos/churn_model/predict.py
@@ -1,30 +1,21 @@
 # %%
 
 import pandas as pd
-import sqlalchemy
 
 model = pd.read_pickle("modelo_rf.pkl")
 model
 # %%
 
-dt = '2024-05-08'
-with open("etl_model.sql", 'r') as open_file:
-    query = open_file.read()
-
-query = query.format(date=dt)
-
-engine = sqlalchemy.create_engine("sqlite:///../../data/database_upsell.db")
+df = pd.read_csv('../data/dados_pontos.csv',sep=';')
+df.head()
 # %%
 
-df = pd.read_sql_query(query, engine)
-df
+X = df[model['features']]
+predict_proba = model['model'].predict_proba(X)[:,1]
+predict_proba
+
+df['prob_active'] = predict_proba
+df[['Name','prob_active']]
 # %%
-
-proba = model['model'].predict_proba(df[model['features']])[:,1]
-proba
-
-
-df['proba_active'] = proba
-(df[['Name', 'proba_active']].sort_values(by='proba_active', ascending=False)
-                             .head(25)
- )
+model
+# %%
